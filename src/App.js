9dc8477f168d561194097b02b16a4faf760a7fe7
fsaberex <69372284+fsaberex@users.noreply.github.com>
2023-12-12 17:55:23 -0500

    Finished Movie app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div class="App-header">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
